6c99b4252ea58c8506a397561c578a6a1c977fbf
riki <shanyulong.riki@bytedance.com>
2023-11-16 21:50:59 +0800

    chapter8: dynamic rendering

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// @see https://prettier.io/docs/en/options
+module.exports = {
+  printWidth: 100,
+  endOfLine: "auto",
+  singleQuote: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  tabWidth: 2,
+  semi: true,
+};
